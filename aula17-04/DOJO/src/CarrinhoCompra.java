464520be27512e72ac8dff2b6bf54f66eac3fe97
ARTHUR PEIXOTO SCHILLER <202402851756@alunos.ibmec.edu.br>
2025-04-17 11:35:10 -0300

    deu ruim

---
+++ aula17-04/DOJO/src/CarrinhoCompra.java
@@ -0,0 +1,14 @@
+public class CarrinhoCompra {
+    private int identificador;
+    private ItemCompra[] item;
+    private int valorTotal;
+
+    public CarrinhoCompra(int identificador) {
+        this.identificador = identificador;
+        this.item = new ItemCompra[];
+    }
+
+    public boolean addProduto() {
+
+    }
+}
